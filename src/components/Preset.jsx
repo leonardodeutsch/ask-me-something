f40af306a9486aa569f91c17161761fb500c9131
Leo <deutschleonardo@gmail.com>
2022-05-20 16:53:51 -0500

    added mobile view

--- src/components/Preset.jsx
+++ src/components/Preset.jsx
@@ -7,7 +7,7 @@ const Preset = ({preset, click}) => {
       <div className="preset-icon" onClick={() => {click(preset)}} style={{background: preset.presetColor}}>
         <i class={preset.presetIcon}></i>
       </div>
-      <div >{preset.presetName}</div>
+      <div className="icon-name">{preset.presetName}</div>
     </div>
   );
 }
